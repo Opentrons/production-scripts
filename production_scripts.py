c7df60fe4cbccee714f9a021122fa88acd0bb246
andiiiiiyy <andy.hu@opentrons.com>
2024-03-29 13:05:57 +0800

    fix heat 96ch build issue

--- production_scripts.py
+++ production_scripts.py
@@ -2,16 +2,16 @@ from ot3_testing.tests.pipette_leveling import PipetteLeveling
 from ot3_testing.tests.test_type import SlotLocationCH96, ChannelDefinitionCH96, ChannelOffsetsCH96, SlotLocationCH8, \
     ChannelOffsetsCH8, ChannelDefinitionCH8
 import asyncio
-from tools.run_heat_96ch_script import heat_96ch
-from ot3_testing.devices.ssh import SSHClient
+# from tools.run_heat_96ch_script import heat_96ch
+# from ot3_testing.devices.ssh import SSHClient
 
 if __name__ == '__main__':
     flex_name = input("输入机器条码: ")
-    ret = input("Test Tools heat 96ch ? (Y/N)")
-    if ret.upper().strip() == "Y":
-        ip_address = input("请输入ip地址:")
-        client = SSHClient(ip_address.strip())
-        heat_96ch(client, flex_name)
+    # ret = input("Test Tools heat 96ch ? (Y/N)")
+    # if ret.upper().strip() == "Y":
+    #     ip_address = input("请输入ip地址:")
+    #     client = SSHClient(ip_address.strip())
+    #     heat_96ch(client, flex_name)
 
     ret = input("Test 96通道移液器平行 ? (Y/N)")
     if ret.upper().strip() == "Y":
