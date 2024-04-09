6e576b131886040897cb07991a586271af5da060
andy <andy.hu.opentrons.com>
2024-04-09 09:28:21 +0800

    apply offset to 8/96ch pipette leveling test

--- production_scripts.py
+++ production_scripts.py
@@ -1,7 +1,8 @@
+import os.path
+
 from ot3_testing.tests.pipette_leveling import PipetteLeveling
-from ot3_testing.test_config.pipette_leveling_config import SlotLocationCH96, ChannelDefinitionCH96, ChannelOffsetsCH96, \
-    SlotLocationCH8, \
-    ChannelOffsetsCH8, ChannelDefinitionCH8
+from ot3_testing.test_config.pipette_leveling_config import SlotLocationCH96, ChannelDefinitionCH96, \
+    SlotLocationCH8,  ChannelDefinitionCH8
 import asyncio
 from tools.inquirer import prompt_flex_name, prompt_test_name, prompt_exit
 from tools import heat_96ch
@@ -10,20 +11,21 @@ from __version__ import get_version
 if __name__ == '__main__':
     get_version()
     flex_name = prompt_flex_name()
+    project_path = os.getcwd()
     while True:
         test_name = prompt_test_name()
         if "heat-96ch" in test_name:
             heat_96ch.test_run()
         elif "leveling-96ch" in test_name:
-            pipette_leveling = PipetteLeveling(SlotLocationCH96, ChannelDefinitionCH96, ChannelOffsetsCH96)
-            asyncio.run(pipette_leveling.run_96ch_test(flex_name))
+            pipette_leveling = PipetteLeveling(SlotLocationCH96, ChannelDefinitionCH96, )
+            asyncio.run(pipette_leveling.run_96ch_test(flex_name, project_path=project_path))
         elif "leveling-8ch" in test_name:
             # run 8
-            pipette_leveling = PipetteLeveling(SlotLocationCH8, ChannelDefinitionCH8, ChannelOffsetsCH8)
+            pipette_leveling = PipetteLeveling(SlotLocationCH8, ChannelDefinitionCH8)
             pipette_leveling.test_name = "8ch"
             pipette_leveling.k = -2
             pipette_leveling.b = 35
-            asyncio.run(pipette_leveling.run_8ch_test(flex_name))
+            asyncio.run(pipette_leveling.run_8ch_test(flex_name, project_path=project_path))
         else:
             pass
 
