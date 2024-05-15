9bc7d2d3915de3aab5d159733279c10024bea83e
andy <andy.hu.opentrons.com>
2024-05-15 14:20:36 +0800

    debug airsensor 2

--- devices/air_sensor2.py
+++ devices/air_sensor2.py
@@ -21,12 +21,14 @@ class AirSensor2:
             pressure = data_list[0].split(' ')[0]
             humidity = data_list[1].split(' ')[0]
             temperature = data_list[2].split(' ')[0]
+            pressure_diffrence = data_list[3].split(' ')[0]
             return {
                 "success": True,
                 "data": {
                     "pressure": pressure,
                     "humidity": humidity,
-                    "temperature": temperature
+                    "temperature": temperature,
+                    "p_difference": pressure_diffrence
                 }
             }
         except Exception as e:
@@ -37,4 +39,6 @@ class AirSensor2:
 if __name__ == '__main__':
     sensor = AirSensor2()
     sensor.connect()
-    sensor.get_air_params()
+    while True:
+        ret = sensor.get_air_params()
+        print(ret)
